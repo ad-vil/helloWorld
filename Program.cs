7eaf7b1f3291e242830df26039ef9e6d05484a35
ad-vil <adil_siddiki@hotmail.com>
2025-02-28 15:52:15 +0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// string fName = "fname";
+// string lName = "lname";
+// // fName = fName.Trim();
+
+// string name = $"First name: {fName} Last name: {lName}";
+
+// Console.WriteLine(name);
+
+// Console.WriteLine(name.Contains("fname"));
+
+// Console.WriteLine(name.Length);
+
+decimal a = 89.23M;
+decimal b = 12.21M;
+
+decimal c = a + b;
+
+Console.WriteLine($"The sum of a and b is {c}");
+
+if (c > 100)
+{
+    Console.WriteLine("the answer is greater than 100");
+}
