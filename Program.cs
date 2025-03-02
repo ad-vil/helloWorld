0736bbf351ec2a9e7e108a524272988e25b7d1f8
ad-vil <adil_siddiki@hotmail.com>
2025-03-02 14:49:17 +0500

    3/2 work

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// initial setup
+
 // Console.WriteLine("Hello, World!");
+
+//basic strings
 
 // string fName = "fname";
 // string lName = "lname";
@@ -13,14 +18,65 @@
 
 // Console.WriteLine(name.Length);
 
-decimal a = 89.23M;
-decimal b = 12.21M;
+// conditionals and loops
 
-decimal c = a + b;
+// decimal a = 89.23M;
+// decimal b = 12.21M;
 
-Console.WriteLine($"The sum of a and b is {c}");
+// decimal c = a + b;
 
-if (c > 100)
-{
-    Console.WriteLine("the answer is greater than 100");
+// bool check = c > 100;
+
+// Console.WriteLine($"The sum of a and b is {c}");
+
+// if (check && a == b)
+// {
+//     Console.WriteLine("the answer is greater than 100");
+//     Console.WriteLine("the values for a and b are equal");
+// } 
+// else if (!check && a == b){
+//     Console.WriteLine("The answer is not greater than 100");
+//     Console.WriteLine("the values for a and b are equal");
+// }
+// else {
+//     Console.WriteLine("The value is not greater than 100");
+//     Console.WriteLine("the values of a and b are not equal");
+// }
+
+// int counter = 0;
+
+// counter++;
+// counter += 2; // counter = 3
+
+// Console.WriteLine($"value of counter is {counter}");
+
+// counter--; // counter == 2
+
+// Console.WriteLine($"curent value of counter is {counter}");
+
+// do{
+//     counter++;
+//     Console.WriteLine($"value of counter is {counter}");
+// } while (counter < 7);
+
+        // arrays and forloops
+
+// int[] array = {1,3,4,5,6};
+
+// foreach (int i in array)
+// {
+//     Console.WriteLine(i);
+// }
+
+// // can also use standard for loops
+// for(int i = 0; i < array.Length; i++){
+//     Console.WriteLine($"current value of i is {i}");
+// }
+
+        // working with List<T>
+
+var names  = new List<String> {"<name>", "John", "Bob"};
+
+foreach (var name in names){
+    Console.WriteLine($"Name: {name.ToUpper()}");
 }
--- obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eaf7b1f3291e242830df26039ef9e6d05484a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
